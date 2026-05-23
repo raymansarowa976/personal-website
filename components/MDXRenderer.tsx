dcc3379033b96cf9e1e43ce5e622b49d0a16a130
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-05-23 11:23:34 -0700

    added image optimization. Not sure if it will be needed

--- components/MDXRenderer.tsx
+++ components/MDXRenderer.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { MDXContent } from '@content-collections/mdx/react'
 import { Pre } from '@/components/Pre'
+import { MdxImage } from '@/components/MdxImage'
 
 const components = {
   pre: Pre,
+  img: MdxImage,
 }
 
 export function MDXRenderer({ code }: { code: string }) {
