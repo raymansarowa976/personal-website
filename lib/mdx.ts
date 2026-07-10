e808ea05270ee22d9f89bde31e842075c7016399
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-07-09 23:02:36 -0700

    removed all blog related stuff

--- lib/mdx.ts
+++
@@ -1,8 +0,0 @@
-import rehypePrettyCode from 'rehype-pretty-code'
-import type { Options } from '@content-collections/mdx'
-
-export const mdxOptions: Options = {
-  rehypePlugins: [
-    [rehypePrettyCode, { theme: 'github-dark' }],
-  ],
-}
